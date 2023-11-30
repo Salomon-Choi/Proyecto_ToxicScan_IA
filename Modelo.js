d7b7833319e7950bbc0ea76f74ca36a3dd188ead
Cesar <cesarperea3@gmail.com>
2023-11-30 12:34:28 -0600

    Agregada la funcion que importa de csv y guarda en una lista los comentarios

--- Modelo.js
+++ Modelo.js
@@ -1,3 +1,31 @@
+// IMPORTANTE: el fs es nativo de las librerias de Node pero hay que instalar el csvtojson usando:
+// npm install --save csvtojson@latest
+
+// Importar dependencias para leer el csv
+const fs = require("fs");
+const csv = require("csvtojson");
+
+// crear una variable global de tipo lista para guardar los comentarios
+let lista = [];
+(async () => {
+    // re iniciar la lista vacia
+    lista = [];
+    // Cargar los comentarios
+    const comments = await csv().fromFile("ListaDeComentarios.csv");
+
+    // recorrer los comentarios y sacar el contenido
+    comments.forEach(comment =>{
+      // desestrcuturar el comentario para sacar su contenido
+      const {comentarios} = comment;
+      // quitar saltos de linea innecesarios
+      const comenString=comentarios.replace(/(\r\n|\n|\r)/gm,"");
+      // agregar a la lista
+      lista.push(comenString)
+    })
+    console.log(lista)
+})();
+
+
 //Implementando el modelo Toxicity Classifier
 
 //importar el modelo de tensorflow
