6569fa3e69b0162afa2cd89a373e9a35a8dea3ee
Cesar <cesarperea3@gmail.com>
2023-12-02 21:46:51 -0600

    Clasificacion del texto en caso que entre en alguna categoria

--- Modelo.js
+++ Modelo.js
@@ -9,7 +9,7 @@ const csv = require("csvtojson");
 let lista = [];
 (async () => {
     // re iniciar la lista vacia
-    lista = [];
+    lista = '';
     // Cargar los comentarios
     const comments = await csv().fromFile("ListaDeComentarios.csv");
 
@@ -20,7 +20,7 @@ let lista = [];
       // quitar saltos de linea innecesarios
       const comenString=comentarios.replace(/(\r\n|\n|\r)/gm,"");
       // agregar a la lista
-      lista.push(comenString)
+      lista = lista + (comenString + " ")
     })
     //Implementando el modelo Toxicity Classifier
 
@@ -38,17 +38,32 @@ let lista = [];
     // //Cargamos el modelo 
 
     toxicity.load(threshold).then(model => {
-      const sentences = ['son of a bitch, jewish fuck'];
-
+      const sentences = ['Fuck you, jewish cunt']; // pa pruebas que si funciona con malas palabras (solo ingles)
+      // sentences.push(lista); //esta parte es para que ingrese el String de los comentarios en el array
+      // console.log(sentences); // solo lo agregue para ver como imprimia el array
       model.classify(sentences).then(predictions => {
 
         predictions = predictions.map(predictions => ({
+          // aqui tomo solo la etiqueta y otros datos en la cual cae el texto
           label: predictions['label'],
           probabilitie: predictions.results[0]['probabilities'][1],
           match: predictions.results[0]['match']
         }))
-        console.log(predictions); 
+        predictions = predictions.filter(p => p.match).map(p => p.label)
+        // evaluo si existe alguna coincidencia con las categorias de toxicidad
+        if (predictions.length == 0){
+          // si no cae en ninguna categoria entonces probablemente no es toxico
+          console.log("probably not toxic")
+        }else{
+          // se verifica que la palabra toxicidad este incluida (aunque es redundante)
+          if (predictions.includes('toxicity')){
+            // si es asi, se imprime como real y se saca del array
+            console.log('Toxicity: TRUE')
+            predictions.pop(predictions.length -1)
+          }
+          // se imprimre en cual categoria podriamos incluir este posible texto toxico
+          console.log("This piece of text might fall into this categories: ",predictions)
+        }
         })
       })
 })();
-
