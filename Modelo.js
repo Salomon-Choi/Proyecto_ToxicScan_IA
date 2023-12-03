7c982faa100e02ec160e42bb30f5da95f3227587
SalomonChoi <Salomon.choi@unah.hn>
2023-12-02 22:08:55 -0600

    Implementacion de modelo

--- Modelo.js
+++ Modelo.js
@@ -8,47 +8,50 @@ const csv = require("csvtojson");
 // crear una variable global de tipo lista para guardar los comentarios
 let lista = [];
 (async () => {
-    // re iniciar la lista vacia
-    lista = [];
-    // Cargar los comentarios
-    const comments = await csv().fromFile("ListaDeComentarios.csv");
 
-    // recorrer los comentarios y sacar el contenido
-    comments.forEach(comment =>{
-      // desestrcuturar el comentario para sacar su contenido
-      const {comentarios} = comment;
-      // quitar saltos de linea innecesarios
-      const comenString=comentarios.replace(/(\r\n|\n|\r)/gm,"");
-      // agregar a la lista
-      lista.push(comenString)
-    })
-    //Implementando el modelo Toxicity Classifier
+  // re iniciar la lista vacia
+  lista = [];
+  // Cargar los comentarios
+  const comments = await csv().fromFile("ListaDeComentarios.csv");
 
-    //importar el modelo de tensorflow
+  // recorrer los comentarios y sacar el contenido
+  comments.forEach(comment => {
+    // desestrcuturar el comentario para sacar su contenido
+    const { comentarios } = comment;
+    // quitar saltos de linea innecesarios
+    const comenString = comentarios.replace(/(\r\n|\n|\r)/gm, "");
+    // agregar a la lista
+    lista.push(comenString)
+  })
+  //Implementando el modelo Toxicity Classifier
 
-
-    require('@tensorflow/tfjs');
-    const toxicity = require('@tensorflow-models/toxicity');
+  //importar el modelo de tensorflow
 
 
-    // //Estableciendo el threshold
-    const threshold = 0.8;
+  require('@tensorflow/tfjs');
+  const toxicity = require('@tensorflow-models/toxicity');
 
 
-    // //Cargamos el modelo 
+  // //Estableciendo el threshold
+  const threshold = 0.8;
 
-    toxicity.load(threshold).then(model => {
-      const sentences = ['son of a bitch, jewish fuck'];
 
-      model.classify(sentences).then(predictions => {
+  // //Cargamos el modelo 
+
+  toxicity.load(threshold).then(model => {
+    const sentences = ['son of a bitch, jewish fuck'];
+
+    model.classify(sentences).then(predictions => {
 
         predictions = predictions.map(predictions => ({
-          label: predictions['label'],
-          probabilitie: predictions.results[0]['probabilities'][1],
-          match: predictions.results[0]['match']
-        }))
-        console.log(predictions); 
-        })
-      })
+        label: predictions['label'],
+        probability: predictions.results[0]['probabilities'][1],
+        match: predictions.results[0]['match']
+      }))
+
+      console.log(predictions);
+    })
+  })
 })();
 
+// $ yarn add @tensorflow/tfjs @tensorflow-models/toxicity
