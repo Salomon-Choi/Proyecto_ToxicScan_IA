0c16b9dcd9263bc4853942e02625d15a85d99811
SalomonChoi <Salomon.choi@unah.hn>
2023-12-03 18:17:03 -0600

    Implementacion del modelo para analizar texto

--- Modelo.js
+++ Modelo.js
@@ -5,8 +5,11 @@
 const fs = require("fs");
 const csv = require("csvtojson");
 
+let categoria
+
 // crear una variable global de tipo lista para guardar los comentarios
 let lista = [];
+
 (async () => {
     // re iniciar la lista vacia
     lista = '';
@@ -22,10 +25,10 @@ let lista = [];
       // agregar a la lista
       lista = lista + (comenString + " ")
     })
+
+    
     //Implementando el modelo Toxicity Classifier
-
     //importar el modelo de tensorflow
-
 
     require('@tensorflow/tfjs');
     const toxicity = require('@tensorflow-models/toxicity');
@@ -35,7 +38,7 @@ let lista = [];
     const threshold = 0.8;
 
 
-    // //Cargamos el modelo 
+/////////////////////////////////Clasificador de toxicidad/////////////////////////////////////////////////////////
 
     toxicity.load(threshold).then(model => {
       const sentences = ['Fuck you, jewish cunt']; // pa pruebas que si funciona con malas palabras (solo ingles)
@@ -46,7 +49,7 @@ let lista = [];
         predictions = predictions.map(predictions => ({
           // aqui tomo solo la etiqueta y otros datos en la cual cae el texto
           label: predictions['label'],
-          probabilitie: predictions.results[0]['probabilities'][1],
+          probability: predictions.results[0]['probabilities'][1],
           match: predictions.results[0]['match']
         }))
         predictions = predictions.filter(p => p.match).map(p => p.label)
@@ -63,9 +66,55 @@ let lista = [];
           }
           // se imprimre en cual categoria podriamos incluir este posible texto toxico
           console.log("This piece of text might fall into this categories: ",predictions)
+          categoria= "This piece of text might fall into this categories: "+predictions
         }
         })
       })
+
+////////////////////////////////////////   MODELO DE ANÁLISIS DE TEXTO //////////////////////////////////////////////////////////
+
+const express = require('express');
+require('dotenv').config()
+const app = express();
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const {
+    TextServiceClient
+} = require("@google-ai/generativelanguage");
+const {
+    GoogleAuth
+} = require("google-auth-library");
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const API_KEY = process.env.API_KEY;
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const client = new TextServiceClient({
+    authClient: new GoogleAuth().fromAPIKey(API_KEY),
+});
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const promptString = "Talk about toxicity on the internet and give an opinion on the following sentence: This piece of text might fall into this categories:  [ 'insult' ]";
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+client.generateText({
+    model: 'models/text-bison-001',
+    temperature: 0.7,
+    candidateCount: 1,
+    top_k: 40,
+    top_p: 0.95,
+    max_output_tokens: 1024,
+    stop_sequences: [],
+    prompt: {
+        text: promptString,
+    },
+}).then(result => {
+    result.forEach(function(d1) {
+        if (d1 != null) {
+            d1.candidates.forEach(function(d2) {
+                console.log(d2.output);
+            })
+        }
+    })
+});
+
 })();
 
 // $ yarn add @tensorflow/tfjs @tensorflow-models/toxicity
+//////////////////////////////////////////////////
+///////////////////////
